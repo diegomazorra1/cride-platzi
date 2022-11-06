0b32d1b8d7618bddad43a0336507c084d3718050
Diego Mazorra <Diego.mazorra@dualbootpartners.com>
2022-11-06 14:04:40 +0100

    video45 commit 34

--- config/settings/base.py
+++ config/settings/base.py
@@ -51,6 +51,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'cride.user',
     'cride.circles',
+    'cride.rides',
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
---
+++ cride/rides/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RidesConfig(AppConfig):
+    name = 'cride.rides'
+    verbose_name = 'Rides'
